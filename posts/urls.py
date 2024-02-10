b8e1a8319b251c4e0f07f459cbdace25e00f445b
eziraa <ezratgab@gmail.com>
2024-02-10 19:53:02 +0300

    Adding post file to see a single post

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('post/<int:pk>', views.post, name='post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,8 @@ from .models import Post
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
+
+
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request, 'post.html', {'post': post})
