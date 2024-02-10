d421b1046106030bf21bf7251ce43106f4a3cea1
eziraa <ezratgab@gmail.com>
2024-02-10 18:54:10 +0300

    adding static files directory

--- Blog/settings.py
+++ Blog/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
-# Default primary key field type
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]  # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
