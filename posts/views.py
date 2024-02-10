a01d54f4175a7d9b4142414da926c80fa52b656f
eziraa <ezratgab@gmail.com>
2024-02-10 18:53:31 +0300

    implenting index method to handle first request

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
