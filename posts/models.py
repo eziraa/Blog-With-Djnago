d034a11331b93822957a3c1ae110dd3eb53667a0
eziraa <ezratgab@gmail.com>
2024-02-10 21:17:39 +0300

    creating ContactMessage model and migrate it

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,10 @@ class Post(models.Model):
     title = models.CharField(max_length=1000)
     body = models.CharField(max_length=1000000)
     created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    written_at = models.DateField(default=datetime.now, blank=True)
